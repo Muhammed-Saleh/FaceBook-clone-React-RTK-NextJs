f025883d0aa407faf1dc0165e8eab3704d96242c
Muhammed-Saleh <hamada500005@gmail.com>
2022-09-04 10:10:26 -0700

    changing the provider to facebook provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLINET_ID,
+      clientSecret: process.env.FACEBOOK_CLINET_SECRET,
     }),
     // ...add more providers here
   ],
