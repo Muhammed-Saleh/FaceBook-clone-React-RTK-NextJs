03057ba62468b1dbbfd95622a1c6e15cc92455e5
Muhammed-Saleh <hamada500005@gmail.com>
2022-09-04 11:10:52 -0700

    Creating Login component & using it in the index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
 
 export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div className="">
       <Head>
