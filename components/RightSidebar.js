c2824b525d5f8cfb2f0c669a3ee92f0727b47752
Muhammed-Saleh <hamada500005@gmail.com>
2022-09-06 17:12:54 -0700

    creating Contacts component and include it in the RightSidebar

--- components/RightSidebar.js
+++ components/RightSidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { RiVideoAddFill } from "react-icons/ri";
 import { BiSearch } from "react-icons/bi";
 import { CgMoreAlt } from "react-icons/cg";
+import Contacts from "./Contacts";
 
 const RightSidebar = () => {
   return (
@@ -20,6 +21,26 @@ const RightSidebar = () => {
           </div>
         </div>
       </div>
+      <Contacts
+        src="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=400"
+        name="Muhammad"
+        status="Online"
+      />
+      <Contacts
+        src="https://images.pexels.com/photos/845457/pexels-photo-845457.jpeg?auto=compress&cs=tinysrgb&w=400"
+        name="Ahmed"
+        status="Online"
+      />
+      <Contacts
+        src="https://images.pexels.com/photos/886285/pexels-photo-886285.jpeg?auto=compress&cs=tinysrgb&w=400"
+        name="Ibrahim"
+        status="Offline"
+      />
+      <Contacts
+        src="https://images.pexels.com/photos/3845457/pexels-photo-3845457.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        name="Hussien"
+        status="Online"
+      />
     </div>
   );
 };
